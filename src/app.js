f0d664c536e202c73b2a5c1351afe67cc6fb71ad
harry <411981379@qq.com>
2017-10-19 17:38:36 +0800

    finish torrent schedule

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ var magnet = 'magnet:?xt=urn:btih:975a12da9b266113ba162620f8c0c68189deab31&dn=De
 var torrentPath = path.normalize('E:\\下载\\【BT吧】[1080p]-神偷奶爸3-卑鄙的我3-坏蛋奖门人3(港)-3.83GB.torrent')
 var magnet2 = 'magnet:?xt=urn:btih:82f6dfe67037d87945a636d6d64d23c7622389bc&dn=Transformers.The.Last.Knight.2017.1080p.WEB-DL.DD5.1.H264-FGT&xl=2082908362&tr=http://tracker.trackerfix.com:80/announce&tr=udp://9.rarbg.me:2710/announce&tr=udp://9.rarbg.to:2710/announce'
 var torrentPath2 = path.normalize('E:\\下载\\【BT吧】[1080p]-变形金刚5：最后的骑士-变形金刚：终极战士(港)-变形金刚5：最终骑士(台)-5.94GB.torrent')
+var magnet3 = 'magnet:?xt=urn:btih:cbae4561e51f2742736e77372b33f386903be581&dn=%E3%80%90BT%E5%90%A7-www.btba.com.cn%E3%80%91%E4%BE%A0%E7%9B%97%E8%81%94%E7%9B%9F.The+Adventurers.2017.1080p%E5%9B%BD%E7%B2%A4%E5%8F%8C%E8%AF%AD%E4%B8%AD%E8%8B%B1%E5%8F%8C%E5%AD%97.BTBA&xl=1787794052&tr=http://tracker.btba.cc:6060/announce&tr=udp://explodie.org:6969/announce&tr=udp://mgtracker.org:2710/announce&tr=udp://9.rarbg.com:2720/announce&tr=udp://shadowshq.yi.org:6969/announce&tr=udp://shadowshq.eddie4.nl:6969/announce&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=http://tracker2.wasabii.com.tw:6969/announce&tr=udp://eddie4.nl:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.leechers-paradise.org:6969/announce'
 
 var torrentBuffer = fs.readFileSync(torrentPath)
 var torrentBuffer2 = fs.readFileSync(torrentPath2)
@@ -20,15 +21,19 @@ client.on('torrent', () => {
 client.on('error', err => {
 	console.log('client trigger error')
 })
-
+var id 
 
 var tempPath = path.normalize('E:\\webTorrentTest\\temp')
 var schedule = new Schedule(tempPath)
-schedule.addTorrent(torrentPath, tempPath)
-// let torrentId = schedule.addMagnet(magnet2, tempPath)
+// schedule.addTorrent(torrentPath, tempPath).then(data => {
+// 	id = data
+// })
+// schedule.addMagnet(magnet2, tempPath)
+// schedule.addMagnet(magnet3, tempPath)
 setTimeout(() => {
 	// console.log('begin pause')
 	// schedule.pause(torrentId)
+	// schedule.destory(id)
 },30000)
 
 setTimeout(() => {
@@ -37,109 +42,8 @@ setTimeout(() => {
 },60000)
 
 setInterval(() => {
-	// console.log(schedule.getSummary(), schedule.getFinish())
-},4000)
-
-
-return
-client.add(torrentBuffer, (torrent) => {
-	let con = () => {
-		console.log(' ')
-		console.log(' ')
-		console.log('torrent infoHash: ' + torrent.infoHash)
-		console.log('torrent magnetURI: ' + torrent.magnetURI)
-		// console.log('torrent torrentFile: ' + torrent.torrentFile)
-		console.log('torrent timeRemaining: ' + torrent.timeRemaining)
-		console.log('torrent received: ' + torrent.received)
-		console.log('torrent downloaded: ' + torrent.downloaded)
-		console.log('torrent uploaded: ' + torrent.uploaded)
-		console.log('torrent downloadSpeed: ' + torrent.downloadSpeed)
-		console.log('torrent uploadSpeed: ' + torrent.uploadSpeed)
-		console.log('torrent progress: ' + torrent.progress)
-		console.log('torrent ratio: ' + torrent.ratio)
-		console.log('torrent numPeers: ' + torrent.numPeers)
-		console.log('torrent path: ' + torrent.path)
-	}
-
-	let cfile = () => {
-		torrent.files.forEach(file => {
-			console.log('')
-			console.log('file name: ' + file.name)
-			console.log('file path: ' + file.path)
-			console.log('file length: ' + file.length)
-			console.log('file downloaded: ' + file.downloaded)
-			console.log('file progress: ' + file.progress)
-		})
-		
-	}
-	let mkv = torrent.files.find(item => item.name.endsWith('mkv'))
-	let txt = torrent.files.find(item => item.name.endsWith('txt'))
-
-	setTimeout(() => {
-		txt.deselect()
-		mkv.deselect()
-		console.log('1')
-	}, 21000)
-
-	setTimeout(() => {
-		txt.select()
-		mkv.select()
-		console.log('2')
-	}, 61000)
-
-	torrent.on('infoHash', () => {
-		console.log('torrent trigger infoHash')
-	})
-
-	torrent.on('metadata', () => {
-		console.log('torrent trigger metadata')
-	})
-
-	torrent.on('ready', () => {
-		console.log('torrent trigger ready')
-	})
-
-	torrent.on('warning', (warning) => {
-		console.log('torrent trigger warning ： ' + warning)
-	})
-
-	torrent.on('error', () => {
-		console.log('torrent trigger error')
-	})
-
-	torrent.on('done', () => {
-		console.log('torrent trigger done') 
-			cfile()
-	})
-
-	torrent.on('download', (bytes) => {
-		// console.log('torrent trigger download')
-	})
-
-	torrent.on('upload', (bytes) => {
-		// console.log('torrent trigger upload')
-	})
-
-	torrent.on('wire', (wire, addr) => {
-		// console.log('torrent trigger wire ' + addr)
-	})
-
-	torrent.on('noPeers', (announceType) => {
-		console.log('torrent trigger noPeers')
-	})
-
-	setInterval(() => {
-		// con()
-		cfile()
-	},20000)
-
-
-	// setTimeout(() => {
-	// 	torrent.destroy(() => {
-	// 		console.log(torrent, 'destroy')
-	// 	},10000)	
-	// })
-})
+	console.log(schedule.getSummary())
+},5000)
 
 
 
--- src/schedule.js
+++ src/schedule.js
@@ -15,7 +15,7 @@ class Schedule extends EventEmitter {
 		this.downloading = []
 		this.downloaded = []
 		this.writing = false
-		this.number = 0
+		this.lockNumber = 0
 		this.log = function() {
 			let infoHash = this.infoHash
 			let timeRemaining = this.timeRemaining
@@ -33,36 +33,50 @@ class Schedule extends EventEmitter {
 		this.init()
 	}
 
+	//read storage & create tasks 
 	init() {
-		if (!fs.existsSync(this.catchPath)) return 
-		let tasks = JSON.parse(fs.readFileSync(this.catchPath))
-		this.downloaded = tasks.downloaded
-		tasks.downloading.forEach(file => {
-			if (file.torrentPath) this.addTorrent(file.torrentPath, file.path)
-			else if (file.magnetURL) this.addMagnet(file.magnetURL, file.path)
-		})
+		if (!fs.existsSync(this.catchPath)) return console.log('catch path not exist')
+		try {
+			let tasks = JSON.parse(fs.readFileSync(this.catchPath))
+			this.downloaded = tasks.downloaded
+			tasks.downloading.forEach((file, index) => {
+				if (file.torrentPath) this.addTorrent(file.torrentPath, file.path)
+				else if (file.magnetURL) this.addMagnet(file.magnetURL, file.path)
+			})
+		}catch(e) {
+			console.log('Error in init ', e)
+			fs.unlinkSync(this.catchPath)
+			console.log('this catch file has been removed')
+		}
 	}
 
-	addTorrent(torrentPath, path) {
+	//add task with torrent file
+	async addTorrent(torrentPath, downloadPath) {
 		if (!fs.existsSync(torrentPath)) throw new Error('torrent file not exist')
 		let torrentBuffer = fs.readFileSync(torrentPath)
-		return this.createTorrent(torrentBuffer, path, torrentPath)
+		return await this.createTorrent(torrentBuffer, downloadPath, torrentPath)
 	}
 
-	addMagnet(magnetURL, path) {
+	//add task with magnet url
+	async addMagnet(magnetURL, downloadPath) {
 		if (typeof magnetURL !== 'string' || magnetURL.indexOf('magnet') == -1) throw new Error('magnetURL is not a legal magnetURL')
-		return this.createTorrent(magnetURL, path)
+		return await this.createTorrent(magnetURL, downloadPath)
 	}
 
-	async createTorrent(t, path, torrentPath) {
-		let torrent = this.client.add(t, {path})
+	// create torrent & storage
+	async createTorrent(torrentSource, downloadPath, torrentPath) {
+		let torrent = this.client.add(torrentSource, {path: this.tempPath})
+		if (this.downloading.findIndex(item => item.infoHash == torrent.infoHash) !== -1) return console.log('torrent exist now')
 		torrent.log = this.log
 		torrent.state = 'downloading'
 		torrent.torrentPath = torrentPath?torrentPath:null
-		torrent.magnetURL = torrentPath?null:t
+		torrent.magnetURL = torrentPath?null:torrentSource
 		torrent.on('done', () => {
 			console.log('torrent done trigger ' + torrent.progress)
+			// pause also will trigger done event
 			if (torrent.progress !== 1) return
+			torrent.state = 'downloaded'
+			// stop torrent uploading & move to downloaded array
 			torrent.destroy(async () => {
 				let index = this.downloading.indexOf(torrent)
 				if (index == -1) throw new Error('torrent is not exist in downloading array')
@@ -73,25 +87,28 @@ class Schedule extends EventEmitter {
 				console.log(this.downloading.length + ' -- ' + this.downloaded.length)
 			})
 		})
-		if (this.downloading.findIndex(item => item.infoHash == torrent.infoHash) !== -1) return console.log('torrent exist now')
 		this.downloading.push(torrent)
 		await this.cache()
 		return torrent.infoHash
 	}
 
+	//pasuse a torrent with torrentID
 	pause(torrentId) {
 		let torrent = this.client.get(torrentId)
 		if (!torrent) return {code: -1}
 		torrent.files.forEach(file => {file.deselect()})
-
+		return {code: 0}
 	}
 
+	//resume a torrent with torrentID
 	resume(torrentId) {
 		let torrent = this.client.get(torrentId)
 		if (!torrent) return {code: -1}
-		torrent.files.forEach(file => {file.select()})		
+		torrent.files.forEach(file => {file.select()})
+		return {code: 0}
 	}
 
+	//query summary of downloading torrent (torrentID is not necessary)
 	getSummary(torrentId) {
 		if (torrentId) {
 			if (typeof torrentId !== 'string' || torrentId.length < 1) throw new Error('torrentId is not legal')
@@ -103,35 +120,37 @@ class Schedule extends EventEmitter {
 		}
 	}
 
+	//query summary of downloaded torrent
 	getFinish() {
 		return this.downloaded.map(file => file.log())
 	}
 
+	//storage list of torrent
 	cache() {
 		return new Promise((resolve,reject) => {
+			console.log('begin cache ...')
 			if (this.writing) {
 				console.log('schedule is writing catche now, waiting...')
 				this.lockNumber++
 				resolve()
+				return
 			}
-
-			console.log('begin cache ...')
-			this.lockNumber = 0
 			this.writing = true
 			let obj = {
 				downloading: this.downloading.map(file => file.log()),
 				downloaded: this.downloaded.map(file => file.log())
 			}
 			fs.writeFile(this.catchPath, JSON.stringify(obj,null, '\t'), err => {
+				
 				this.writing = false
 				if (err) {
 					console.log('cache tasks failed')
 					reject(err)
 				}
 				else {
-					console.log('cache tasks success')
+					console.log('cache tasks success, lockNumber: ' + this.lockNumber)
 					if (this.lockNumber > 0) {
-						console.log('exist writing task todo')
+						this.lockNumber = 0
 						this.cache()
 					}
 					resolve()
@@ -140,8 +159,30 @@ class Schedule extends EventEmitter {
 		})
 	}
 
+	//delete torrent task
 	destory(torrentId) {
-		
+		return new Promise(async (resolve, reject) => {
+			let torrent = this.downloading.find(item => item.infoHash == torrentId)
+			if (torrent) {
+				torrent.destroy(async () => {
+					console.log('torrent has been destory')
+					let index = this.downloading.indexOf(torrent)
+					console.log('index in downloading is : ' + index)
+					this.downloading.splice(index, 1)
+					await this.cache()
+					resolve()
+				})
+			}else {
+				torrent = this.downloaded.find(item => item.infoHash == torrent)
+				if (!torrent) reject()
+				let index = this.downloaded.indexOf(torrent)
+				console.log('index in downloaded is : ' + index)
+				this.downloaded.splice(index, 1)
+				await this.cache()
+				resolve()
+			}
+		})
+
 	}
 
 	clientError() {
